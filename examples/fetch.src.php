be8ed480c365d8cb4e4c72452114e7644baf2e49
Momchil Bozhinov <momchil@bozhinov.eu>
2024-12-15 18:30:35 +0200

    * FILTER_SANITIZE_STRING depreciated

--- examples/fetch.src.php
+++ examples/fetch.src.php
@@ -1,6 +1,6 @@
 <?php
 if (isset($_POST["View"])){
-	$f = filter_var($_POST["View"], FILTER_SANITIZE_STRING);
+	$f = filter_var($_POST["View"], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 	if (file_exists("example.".$f.".php")){
 		highlight_file("example.".$f.".php"); 
 	}
